e76af26254b63a27f65cb40f59541e1c85415876
hunghx <baby10051999@gmail.com>
2023-06-26 15:59:24 +0700

    update 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world Hung111!");
+        System.out.println("Hello world Hung 1234567" + "!");
     }
 }
